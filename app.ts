f949746be7e58f42a37112524a2e51e01bc45ad0
NeoQasim <139628391+NeoQasim@users.noreply.github.com>
2023-09-08 23:35:35 +0500

    deep dive

--- app.ts
+++ app.ts
@@ -34,63 +34,63 @@ function print(id: ID) {
 print('23')
 
 
-// interface Transaction {
-//     payeeraccountnumber: number
-//     payeeaccount: number
+interface Transaction {
+    payeeraccountnumber: number
+    payeeaccount: number
 
-// }
-// interface BankAccount {
-//     acountnumber: number
-//     accountholder: string
-//     balance: number
-//     isactive: boolean
-//     transaction: Transaction[]
-// }
-// const transaction1: Transaction = {
-//     payeeaccount: 1,
-//     payeeraccountnumber: 23
-// }
-// const transaction2: Transaction = {
-//     payeeaccount: 11,
-//     payeeraccountnumber: 231
-// }
-// const bankAccount: BankAccount = {
-//     acountnumber: 1,
-//     accountholder: "me",
-//     balance: 2000,
-//     isactive: true,
-//     transaction: [transaction2]
-// }
-// console.log(bankAccount);
+}
+interface BankAccount {
+    acountnumber: number
+    accountholder: string
+    balance: number
+    isactive: boolean
+    transaction: Transaction[]
+}
+const transaction1: Transaction = {
+    payeeaccount: 1,
+    payeeraccountnumber: 23
+}
+const transaction2: Transaction = {
+    payeeaccount: 11,
+    payeeraccountnumber: 231
+}
+const bankAccount: BankAccount = {
+    acountnumber: 1,
+    accountholder: "me",
+    balance: 2000,
+    isactive: true,
+    transaction: [transaction2]
+}
+console.log(bankAccount);
 
-// interface Bookinfo {
-//     title: string,
-//     author: string,
-//     price: number,
-// }
-// interface EBookinfo extends Bookinfo {
-//     file_size: number
-//     file_format: string
+interface Bookinfo {
+    title: string,
+    author: string,
+    price: number,
+}
+interface EBookinfo extends Bookinfo {
+    file_size: number
+    file_format: string
 
-// }
+}
 
-// // interface Audiobooks extends EBookinfo {
-// //     vocals: string
+interface Audiobooks extends EBookinfo {
+    vocals: string
 
-// // }
-// // const eBook1: EBookinfo = {
-// //     title: "Title here",
-// //     author: "Author name",
-// //     price: 350,
-// //     file_format: "pdf",
-// //     file_size: 1024
-// // }
-// // const aBook1: Audiobooks = {
-// //     title: "Audio book title ",
-// //     author: "Audiobooks Author",
-// //     price: 456,
-// //     file_size: 1024,
-// //     vocals: "Audio vocals",
-// //     file_format: "mp3"
-// // }
+}
+const eBook1: EBookinfo = {
+    title: "Title here",
+    author: "Author name",
+    price: 350,
+    file_format: "pdf",
+    file_size: 1024
+}
+const aBook1: Audiobooks = {
+    title: "Audio book title ",
+    author: "Audiobooks Author",
+    price: 456,
+    file_size: 1024,
+    vocals: "Audio vocals",
+    file_format: "mp3"
+}
 
