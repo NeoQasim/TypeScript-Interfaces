f5833fbc870d7309c9094fa3d31a89fcb1b6d116
NeoQasim <139628391+NeoQasim@users.noreply.github.com>
2023-09-08 23:33:03 +0500

    deep dive

--- app.js
+++ app.js
@@ -8,40 +8,70 @@ const user2 = {
     age: 21
 };
 function login(userdata) {
-    console.log(userdata);
+    // console.log(userdata)
 }
 login(user);
-const transaction1 = {
-    payeeaccount: 1,
-    payeeraccountnumber: 23
-};
-const transaction2 = {
-    payeeaccount: 11,
-    payeeraccountnumber: 231
-};
-const bankAccount = {
-    acountnumber: 1,
-    accountholder: "me",
-    balance: 2000,
-    isactive: true,
-    transaction: [transaction2]
-};
-console.log(bankAccount);
-const eBook1 = {
-    title: "Title here",
-    author: "Author name",
-    price: 350,
-    file_format: "pdf",
-    file_size: 1024
-};
-const aBook1 = {
-    title: "Audio book title ",
-    author: "Audiobooks Author",
-    price: 456,
-    file_size: 1024,
-    vocals: "Audio vocals",
-    file_format: "mp3"
-};
-console.log(eBook1);
-console.log(aBook1);
+function print(id) {
+    if (typeof id === "string") {
+        console.log(id.toString(), "=====ssssssssssssssss========");
+    }
+    else {
+        console.log(id);
+    }
+}
+print('23');
 export {};
+// interface Transaction {
+//     payeeraccountnumber: number
+//     payeeaccount: number
+// }
+// interface BankAccount {
+//     acountnumber: number
+//     accountholder: string
+//     balance: number
+//     isactive: boolean
+//     transaction: Transaction[]
+// }
+// const transaction1: Transaction = {
+//     payeeaccount: 1,
+//     payeeraccountnumber: 23
+// }
+// const transaction2: Transaction = {
+//     payeeaccount: 11,
+//     payeeraccountnumber: 231
+// }
+// const bankAccount: BankAccount = {
+//     acountnumber: 1,
+//     accountholder: "me",
+//     balance: 2000,
+//     isactive: true,
+//     transaction: [transaction2]
+// }
+// console.log(bankAccount);
+// interface Bookinfo {
+//     title: string,
+//     author: string,
+//     price: number,
+// }
+// interface EBookinfo extends Bookinfo {
+//     file_size: number
+//     file_format: string
+// }
+// // interface Audiobooks extends EBookinfo {
+// //     vocals: string
+// // }
+// // const eBook1: EBookinfo = {
+// //     title: "Title here",
+// //     author: "Author name",
+// //     price: 350,
+// //     file_format: "pdf",
+// //     file_size: 1024
+// // }
+// // const aBook1: Audiobooks = {
+// //     title: "Audio book title ",
+// //     author: "Audiobooks Author",
+// //     price: 456,
+// //     file_size: 1024,
+// //     vocals: "Audio vocals",
+// //     file_format: "mp3"
+// // }
--- app.ts
+++ app.ts
@@ -16,72 +16,81 @@ const user2: User = {
 }
 
 function login(userdata: User) {
-    console.log(userdata)
+    // console.log(userdata)
 }
 login(user)
 
+type ID = number | string
+function print(id: ID) {
 
-
-
-interface Transaction {
-    payeeraccountnumber: number
-    payeeaccount: number
-
-}
-interface BankAccount {
-    acountnumber: number
-    accountholder: string
-    balance: number
-    isactive: boolean
-    transaction: Transaction[]
-}
-const transaction1: Transaction = {
-    payeeaccount: 1,
-    payeeraccountnumber: 23
-}
-const transaction2: Transaction = {
-    payeeaccount: 11,
-    payeeraccountnumber: 231
-}
-const bankAccount: BankAccount = {
-    acountnumber: 1,
-    accountholder: "me",
-    balance: 2000,
-    isactive: true,
-    transaction: [transaction2]
-}
-console.log(bankAccount);
-
-interface Bookinfo {
-    title: string,
-    author: string,
-    price: number,
-}
-interface EBookinfo extends Bookinfo {
-    file_size: number
-    file_format: string
-
+    if (typeof id === "string") {
+        console.log(id.toString(), "=====ssssssssssssssss========");
+    }
+    else {
+        console.log(id);
+    }
 }
 
-interface Audiobooks extends EBookinfo {
-    vocals: string
+print('23')
 
-}
-const eBook1: EBookinfo = {
-    title: "Title here",
-    author: "Author name",
-    price: 350,
-    file_format: "pdf",
-    file_size: 1024
-}
-const aBook1: Audiobooks = {
-    title: "Audio book title ",
-    author: "Audiobooks Author",
-    price: 456,
-    file_size: 1024,
-    vocals: "Audio vocals",
-    file_format: "mp3"
-}
 
-console.log(eBook1);
-console.log(aBook1);
+// interface Transaction {
+//     payeeraccountnumber: number
+//     payeeaccount: number
+
+// }
+// interface BankAccount {
+//     acountnumber: number
+//     accountholder: string
+//     balance: number
+//     isactive: boolean
+//     transaction: Transaction[]
+// }
+// const transaction1: Transaction = {
+//     payeeaccount: 1,
+//     payeeraccountnumber: 23
+// }
+// const transaction2: Transaction = {
+//     payeeaccount: 11,
+//     payeeraccountnumber: 231
+// }
+// const bankAccount: BankAccount = {
+//     acountnumber: 1,
+//     accountholder: "me",
+//     balance: 2000,
+//     isactive: true,
+//     transaction: [transaction2]
+// }
+// console.log(bankAccount);
+
+// interface Bookinfo {
+//     title: string,
+//     author: string,
+//     price: number,
+// }
+// interface EBookinfo extends Bookinfo {
+//     file_size: number
+//     file_format: string
+
+// }
+
+// // interface Audiobooks extends EBookinfo {
+// //     vocals: string
+
+// // }
+// // const eBook1: EBookinfo = {
+// //     title: "Title here",
+// //     author: "Author name",
+// //     price: 350,
+// //     file_format: "pdf",
+// //     file_size: 1024
+// // }
+// // const aBook1: Audiobooks = {
+// //     title: "Audio book title ",
+// //     author: "Audiobooks Author",
+// //     price: 456,
+// //     file_size: 1024,
+// //     vocals: "Audio vocals",
+// //     file_format: "mp3"
+// // }
+
